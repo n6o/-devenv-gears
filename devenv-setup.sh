da71aa20695b3cd59efa248d0a9eaee7a48f2c47
n6o <16708063+n6o@users.noreply.github.com>
2022-10-12 10:51:40 +0900

    Update devenv-setup

--- devenv-setup.sh
+++ devenv-setup.sh
@@ -2,6 +2,6 @@
 
 ARCHIVE_DIR=~/archive
 
-mkdir ${ARCHIVE_DIR}
+mkdir -p ${ARCHIVE_DIR}
 curl -o ${ARCHIVE_DIR}/devenv.dist.yml https://raw.githubusercontent.com/n6o/virt-playbooks/main/devenv.dist.yml
 ansible-playbook ${ARCHIVE_DIR}/devenv.dist.yml
